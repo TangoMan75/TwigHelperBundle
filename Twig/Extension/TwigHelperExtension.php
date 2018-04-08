3a02da16da77d758b9d4539384c128858efb8e77
Matthias Morin <matthias.morin@gmail.com>
2018-04-08 23:21:03 +0200

    Reformat + Copyright + README

--- Twig/Extension/TwigHelperExtension.php
+++ Twig/Extension/TwigHelperExtension.php
@@ -1,9 +1,16 @@
 <?php
+/**
+ * Copyright (c) 2018 Matthias Morin <matthias.morin@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace TangoMan\TwigHelperBundle\Twig\Extension;
 
 class TwigHelperExtension extends \Twig_Extension
 {
+
     /**
      * @var \Twig_Environment
      */
@@ -33,9 +40,13 @@ class TwigHelperExtension extends \Twig_Extension
     public function getFunctions()
     {
         return [
-            new \Twig_SimpleFunction('start_with', [$this, 'startWithFunction']),
+            new \Twig_SimpleFunction(
+                'start_with', [$this, 'startWithFunction']
+            ),
             new \Twig_SimpleFunction('end_with', [$this, 'endWithFunction']),
-            new \Twig_SimpleFunction('is_numeric', [$this, 'isNumericFunction']),
+            new \Twig_SimpleFunction(
+                'is_numeric', [$this, 'isNumericFunction']
+            ),
             new \Twig_SimpleFunction('is_string', [$this, 'isStringFunction']),
         ];
     }
